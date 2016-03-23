e0e79c1e153b53ce2282e2fb614bd1def72e03df
Matt Long <matt@burythehammer.com>
2016-03-23 14:03:03 +0000

    Support added for email sending and checking

---
+++ api-acceptance-tests/src/test/java/com/opencredo/test/api/acceptance/test/step/definitions/EmailStepDefs.java
@@ -0,0 +1,39 @@
+package com.opencredo.test.api.acceptance.test.step.definitions;
+
+
+import com.opencredo.test.EmailAdaptor;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import javax.mail.Message;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EmailStepDefs extends AbstractStepDefinition {
+
+    @When("^I send a test email$")
+    public void iSendATestEmailTo() throws Throwable {
+        new EmailAdaptor().connect().sendTestEmail();
+    }
+
+    @Then("^there should be '(\\d+)' test (?:email|emails) sent$")
+    public void thereShouldBeTestEmailsSentTo(int emailCount) throws Throwable {
+
+        Thread.sleep(30000); // wait for emails to be sent
+
+        final EmailAdaptor emailAdaptor = new EmailAdaptor();
+        final List<Message> messages = emailAdaptor.connect().getRecentEmails();
+        emailAdaptor.disconnect();
+
+        assertThat(messages.size()).isEqualTo(emailCount);
+    }
+
+    @Given("^test emails have been deleted from my inbox$")
+    public void testEmailsHaveBeenDeletedFromMyAccount() throws Throwable {
+        final EmailAdaptor emailAdaptor = new EmailAdaptor();
+        final List<Message> messages = emailAdaptor.connect().deleteTestEmails();
+    }
+}
