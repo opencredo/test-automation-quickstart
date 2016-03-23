62f21f14e1a3fe0ffa343e94b7a02f598c07afa2
Matt Long <matt@burythehammer.com>
2016-03-23 14:57:40 +0000

    Test now sets up data before test so that result is always the same.

--- api-acceptance-tests/src/test/java/com/opencredo/test/api/acceptance/test/step/definitions/EmailStepDefs.java
+++ api-acceptance-tests/src/test/java/com/opencredo/test/api/acceptance/test/step/definitions/EmailStepDefs.java
@@ -2,7 +2,6 @@ package com.opencredo.test.api.acceptance.test.step.definitions;
 
 
 import com.opencredo.test.EmailAdaptor;
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -19,21 +18,21 @@ public class EmailStepDefs extends AbstractStepDefinition {
         new EmailAdaptor().connect().sendTestEmail();
     }
 
-    @Then("^there should be '(\\d+)' test (?:email|emails) sent$")
+    @Then("^there should be '(\\d+)' test (?:email|emails) in my inbox$")
     public void thereShouldBeTestEmailsSentTo(int emailCount) throws Throwable {
 
         Thread.sleep(30000); // wait for emails to be sent
 
         final EmailAdaptor emailAdaptor = new EmailAdaptor();
-        final List<Message> messages = emailAdaptor.connect().getRecentEmails();
+        final List<Message> messages = emailAdaptor.connect().getTestEmails();
         emailAdaptor.disconnect();
 
         assertThat(messages.size()).isEqualTo(emailCount);
     }
 
-    @Given("^test emails have been deleted from my inbox$")
+    @Given("^test emails have been deleted from my account$")
     public void testEmailsHaveBeenDeletedFromMyAccount() throws Throwable {
         final EmailAdaptor emailAdaptor = new EmailAdaptor();
-        final List<Message> messages = emailAdaptor.connect().deleteTestEmails();
+        emailAdaptor.connect().deleteTestEmails();
     }
 }
--- api-acceptance-tests/src/test/resources/cucumber/EmailApiTest.feature
+++ api-acceptance-tests/src/test/resources/cucumber/EmailApiTest.feature
@@ -1,7 +1,9 @@
 @api-demo
 Feature: Demonstrate the Email API acceptance testing framework
 
+  # Before running these tests, adjust the email settings in EmailAdaptor.java to your email account
+
   Scenario: Simple email interaction
     Given test emails have been deleted from my account
     When I send a test email
-    Then there should be '1' test email sent
+    Then there should be '1' test email in my inbox
