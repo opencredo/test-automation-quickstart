78bf0b05d57e527e3c3455b6deda76e2cad64501
Carlos Lindo <carlos.lindo@opencredo.com>
2017-03-30 16:32:51 +0100

    Removed security tests package from ui tests module

--- ui-acceptance-tests/src/test/java/com/opencredo/test/security/acceptance/test/config/TestWorld.java
+++
@@ -1,7 +0,0 @@
-package com.opencredo.test.security.acceptance.test.config;
-
-import java.util.HashMap;
-
-public class TestWorld {
-    public HashMap<String, String> sharedState = new HashMap<>();
-}
